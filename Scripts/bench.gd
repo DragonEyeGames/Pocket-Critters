93ee76e70653a7c4bb1e903dc6484b1c15a7ddb7
Taxalottle Syndicate <bthole41@gmail.com>
2026-03-27 10:32:43 -0400

    Minamai + Side Bench
    I put in minamia (new pokemon) as well as a new system for the bench. Now we can rest al over! (except up)

--- Scripts/bench.gd
+++ Scripts/bench.gd
@@ -3,9 +3,42 @@ extends Node2D
 var playerEntered=false
 var player
 var inProgress=false
+
+enum directions {
+	Front,
+	Left,
+	Right
+}
+
+@export var direction: directions
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	match direction:
+		directions.Front:
+			$Right.queue_free()
+			$Left.queue_free()
+			for child in $Front.get_children():
+				var globalPos=child.global_position
+				$Front.call_deferred("remove_child", child)
+				call_deferred("add_child", child)
+				child.set_deferred("global_position", globalPos)
+		directions.Right:
+			$Front.queue_free()
+			$Left.queue_free()
+			for child in $Right.get_children():
+				var globalPos=child.global_position
+				$Right.call_deferred("remove_child", child)
+				call_deferred("add_child", child)
+				child.set_deferred("global_position", globalPos)
+		directions.Left:
+			$Front.queue_free()
+			$Right.queue_free()
+			for child in $Left.get_children():
+				var globalPos=child.global_position
+				$Left.call_deferred("remove_child", child)
+				call_deferred("add_child", child)
+				child.set_deferred("global_position", globalPos)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,16 +48,37 @@ func _process(_delta: float) -> void:
 		GameManager.canPause=false
 		inProgress=true
 		$StaticBody2D/CollisionShape2D.set_deferred("disabled", true)
-		player.sprite.play("up-walk")
-		player.direction="up"
-		player.state="walk"
+		match direction:
+			directions.Front:
+				player.sprite.play("up-walk")
+				player.direction="up"
+				player.state="walk"
+			directions.Right:
+				player.sprite.play("left-walk")
+				player.direction="left"
+				player.state="walk"
+			directions.Left:
+				player.sprite.play("right-walk")
+				player.direction="right"
+				player.state="walk"
 		player.overrideWalking=true
 		GameManager.respawnSpot=Vector2(global_position.x, global_position.y+1)
 		var tween=create_tween()
 		tween.tween_property(player, "global_position", Vector2(global_position.x, global_position.y+1), .5)
 		await tween.finished
-		player.sprite.play("down-idle")
-		player.direction="down"
+		match direction:
+			directions.Front:
+				player.sprite.play("down-idle")
+				player.direction="down"
+				player.state="idle"
+			directions.Right:
+				player.sprite.play("right-idle")
+				player.direction="right"
+				player.state="idle"
+			directions.Left:
+				player.sprite.play("left-idle")
+				player.direction="left"
+				player.state="idle"
 		player.overrideWalking=false
 		await get_tree().create_timer(.5).timeout
 		var tween2=create_tween()
