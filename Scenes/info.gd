c41383dea7437761ca235698a00e75c73c756cf3
Taxalottle Syndicate <bthole41@gmail.com>
2026-04-13 19:27:18 -0400

    Pokedex Descriptions
    I did some more work on the pokedex by putting in descriptions. I think it will take a very long time to right all of those though.

--- Scenes/info.gd
+++ Scenes/info.gd
@@ -3,8 +3,32 @@ extends CanvasLayer
 @export var species: SpeciesData
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	initialize()
+
+func initialize():
 	print(species.species)
 	$PokemonHolder/NewPokemon.pokemon=species.species
 	$PokemonHolder/NewPokemon.initialize()
 	$Name.text=GameManager.pokemonName(species.species)
+	$Name.text+=" #" + str(species.species+1).pad_zeros(2)
+	$Title/title.text=PokedexText.pokemonTitle[species.species]
+	$Description/description.text=PokedexText.pokemonDescription[species.species]
+	$Types/Type.type=species.type1
+	$Types/Type.initialize()
+	$Types/Type.visible=true
+	$Types/Type2.visible=true
 	
+	if(species.type2!=species.type1):
+		$Types/Type2.type=species.type2
+		$Types/Type2.initialize()
+	else:
+		$Types/Type2.visible=false
+	if(species.species not in GameManager.seenDex):
+		$PokemonHolder/NewPokemon.modulate=Color.BLACK
+		$Title/title.text="???"
+		$Description/description.text="???"
+		$Types/Type.visible=false
+		$Types/Type2.visible=false
+	
+func close():
+	visible=false
---
+++ Scripts/pokedexText.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var pokemonTitle = {
+	GameManager.pokemon.Timberry: "Berry Pup Critter",
+	GameManager.pokemon.Howliage: "Berry Wolf Critter",
+	GameManager.pokemon.Botanine: "Large Foliage Critter"
+}
+
+var pokemonDescription = {
+	GameManager.pokemon.Timberry: "Timberry loves to frollick in forests. It can often be found near berry bushes, getting a light snack. It is a common first critter among yougsters because of its cute appearance.",
+	GameManager.pokemon.Howliage: "Howliage start to assume more responsibilities in its packs. It now has to gather berries for it's pack. This critter can often be seen eating berries, as they are its favorite snack.",
+	GameManager.pokemon.Botanine: "Botanine is the leader of its pack. It takes care of the Timberrys and sends the howliage on missions. Without a botanine, the pack would quickly fall apart."
+}
