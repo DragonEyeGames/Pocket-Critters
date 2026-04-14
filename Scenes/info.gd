b162e96c58ad23a26c7ab58b7733945bbc69237c
Taxalottle Syndicate <bthole41@gmail.com>
2026-04-13 20:50:20 -0400

    Forwards and backwards
    I put in forwards and backwards movement for scrolling the pokedex. I'll do some more work on it tomorrow.

--- Scenes/info.gd
+++ Scenes/info.gd
@@ -6,13 +6,21 @@ func _ready() -> void:
 	initialize()
 
 func initialize():
+	if(species==null):
+		return
 	print(species.species)
 	$PokemonHolder/NewPokemon.pokemon=species.species
 	$PokemonHolder/NewPokemon.initialize()
 	$Name.text=GameManager.pokemonName(species.species)
 	$Name.text+=" #" + str(species.species+1).pad_zeros(2)
-	$Title/title.text=PokedexText.pokemonTitle[species.species]
-	$Description/description.text=PokedexText.pokemonDescription[species.species]
+	if(species.species < PokedexText.pokemonTitle.size()):
+		$Title/title.text=PokedexText.pokemonTitle[species.species]
+	else:
+		$Title/title.text="Unwritten Critter"
+	if(species.species < PokedexText.pokemonDescription.size()):
+		$Description/description.text=PokedexText.pokemonDescription[species.species]
+	else:
+		$Description/description.text="Unwritten. Mr. Taxalottle has to get on that."
 	$Types/Type.type=species.type1
 	$Types/Type.initialize()
 	$Types/Type.visible=true
@@ -27,8 +35,34 @@ func initialize():
 		$PokemonHolder/NewPokemon.modulate=Color.BLACK
 		$Title/title.text="???"
 		$Description/description.text="???"
-		$Types/Type.visible=false
-		$Types/Type2.visible=false
+		#$Types/Type.visible=false
+		#$Types/Type2.visible=false
 	
 func close():
 	visible=false
+
+
+func nextPage() -> void:
+	if(species==null):
+		return
+	var speciesInt=species.species+1
+	if(speciesInt>=GameManager.pokemon.size()):
+		return
+	print(GameManager.pokemonName(speciesInt))
+	if(ResourceLoader.exists("res://Pokemon/" + str(GameManager.pokemonName(speciesInt)).to_lower() + ".tres")):
+		species=load("res://Pokemon/" + str(GameManager.pokemonName(speciesInt)).to_lower() + ".tres")
+	else:
+		species.species=species.species+1 as GameManager.pokemon
+	initialize()
+
+
+func lastPage() -> void:
+	var speciesInt=species.species-1
+	if(speciesInt<0):
+		return
+	print(GameManager.pokemonName(speciesInt))
+	if(ResourceLoader.exists("res://Pokemon/" + str(GameManager.pokemonName(speciesInt)).to_lower() + ".tres")):
+		species=load("res://Pokemon/" + str(GameManager.pokemonName(speciesInt)).to_lower() + ".tres")
+	else:
+		species.species=species.species-1 as GameManager.pokemon
+	initialize()
--- Scenes/type.gd
+++ Scenes/type.gd
@@ -3,4 +3,9 @@ extends Node2D
 @export var type: GameManager.types
 
 func initialize():
-	$TypeAnimator.play(GameManager.types.keys()[type])
+	for child in $Types.get_children():
+		child.visible=false
+	if($TypeAnimator.has_animation(GameManager.types.keys()[type])):
+		$TypeAnimator.play(GameManager.types.keys()[type])
+	else:
+		$TypeAnimator.play("Nonexistent")
