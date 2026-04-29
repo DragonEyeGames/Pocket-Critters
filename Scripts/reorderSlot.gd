bcfe3098689fa33dedb46651c9ed858d3c25082e
Taxalottle Syndicate <bthole41@gmail.com>
2026-04-29 17:01:44 -0400

    Improved theme congruency
    I improved my game by making atleast most of the fonts pixel. Theres prob a few stragglers hiding tho.

--- Scripts/reorderSlot.gd
+++ Scripts/reorderSlot.gd
@@ -13,6 +13,7 @@ func initialize():
 		$NewPokemon.pokemon=pokemon.species
 		$NewPokemon.initialize()
 		$Name.text=pokemon.name
+		fitText()
 		$"Health Bar".value=pokemon.health
 		$"Health Bar".max_value=pokemon.maxHealth
 		$"Health Bar/Health".text=str(pokemon.health) + "/" + str(pokemon.maxHealth)
@@ -21,6 +22,18 @@ func initialize():
 	else:
 		visible=false
 
+func fitText():
+	var font = $Name.get_theme_font("font")
+	var sizes = 16  # your pixel font size
+
+	var text_width = font.get_string_size($Name.text, HORIZONTAL_ALIGNMENT_LEFT, -1, sizes).x
+	var text_height = font.get_height(sizes)
+	var widthPercent = 125/text_width
+	var heightPercent = 24/text_height
+	var largestVal = min(widthPercent, heightPercent)
+	largestVal/=.9
+	$Name.scale=Vector2(largestVal, largestVal)
+	#$Name.scale = Vector2(text_width + 20, text_height + 10)
 
 func _on_select_pressed() -> void:
 	bigBoss.swapPokemon(index)
--- Scripts/replaceSlot.gd
+++ Scripts/replaceSlot.gd
@@ -18,8 +18,22 @@ func initialize():
 		$"Health Bar/Health".text=str(pokemon.health) + "/" + str(pokemon.maxHealth)
 		$Button.disabled=(pokemon.health<=0 or bigBoss.activeIndex==index)
 		$Fainted.visible=pokemon.health<=0
+		fitText()
 	else:
 		visible=false
+
+func fitText():
+	var font = $Name.get_theme_font("font")
+	var sizes = 16  # your pixel font size
+
+	var text_width = font.get_string_size($Name.text, HORIZONTAL_ALIGNMENT_LEFT, -1, sizes).x
+	var text_height = font.get_height(sizes)
+	var widthPercent = 106/text_width
+	var heightPercent = 18/text_height
+	var largestVal = min(widthPercent, heightPercent)
+	largestVal/=.9
+	$Name.scale=Vector2(largestVal, largestVal)
+	#$Name.scale = Vector2(text_width + 20, text_height + 10)
 
 
 func _on_select_pressed() -> void:
